4f60c7a936732423c903376855c5ce724d5640f8
Qian Zhang <qzhang@agleader.com>
2022-11-18 11:15:32 -0600

    Remove try/catch

--- SignalRInvoke/Hubs/ChatHub.cs
+++ SignalRInvoke/Hubs/ChatHub.cs
@@ -8,17 +8,10 @@ namespace SignalRInvoke.Hubs
         {
             Console.WriteLine("Hub.InvokeMessage: method entry.");
             // System.Exception thrown here after the client returned bool value.
-            try
-            {
-                bool result =
+            bool result =
                     await Clients.Caller.InvokeAsync<bool>("ReceiveMessageInvoke", user, message,
                         CancellationToken.None);
                 Console.WriteLine($"Hub.InvokeMessage: method exit. Result: {result}.");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"Hub.InvokeMessage: method exit. Exception: {e.Message}.");
-            }
         }
     }
 }
